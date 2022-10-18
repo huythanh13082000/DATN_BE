c4a646328b3d3b5f30fb3333e1407a9736d4f121
HuyThanh <huythanh1308bn@gmail.com>
2022-10-18 17:58:44 +0700

    update 18/10

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose');
 const userRoute = require('./api/routes/user.route')
 const bodyParser = require('body-parser')
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const uploadFileRoute = require('./api/routes/uploadFile.route');
 
@@ -12,12 +12,13 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(bodyParser.urlencoded({ extended: true }))
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
   }
 })
 
@@ -32,7 +33,7 @@ async function main() {
 
 app.use('/api', userRoute)
 app.use('/api', uploadFileRoute)
-app.post('/stats', upload.single('uploaded_file'), function (req, res) {
+app.post('/uploadFile', upload.single('avatar'), function (req, res) {
   // req.file is the name of your file in the form above, here 'uploaded_file'
   // req.body will hold the text fields, if there were any 
   console.log(req.file, req.body)
