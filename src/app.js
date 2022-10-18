23f4844116563ca548476a487d5ea8128a81a8d4
ThànhPH <huythanh1308bn@gmail.com>
2022-10-18 23:27:54 +0700

    update 18/10

--- src/app.js
+++ src/app.js
@@ -4,23 +4,27 @@ const mongoose = require('mongoose');
 const userRoute = require('./api/routes/user.route')
 const bodyParser = require('body-parser')
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const uploadFileRoute = require('./api/routes/uploadFile.route');
 
 const app = express()
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-const storage = multer.diskStorage({
+
+// SET STORAGE
+var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    const fileName = file.fieldname + '-' + Date.now() + '.png'
+    cb(null, fileName)
   }
 })
+
+var upload = multer({ storage: storage })
 
 // app.use(cors());
 // create application/x-www-form-urlencoded parser
