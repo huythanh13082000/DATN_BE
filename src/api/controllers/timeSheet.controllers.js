2d16fafb3664cb45e17176fbbb504906c0eccd5d
HuyThanh <huythanh1308bn@gmail.com>
2022-11-11 17:47:34 +0700

    update 11/11

--- src/api/controllers/timeSheet.controllers.js
+++ src/api/controllers/timeSheet.controllers.js
@@ -1,11 +1,30 @@
+const moment = require("moment/moment")
 const personnelModel = require("../models/personnel.model")
 const timeSheetModel = require("../models/timeSheet.model")
+
 
 const createTimeSheetMany = async (req, res) => {
   try {
     const data = req.body
-    const timeSheets = await timeSheetModel.insertMany(data)
-    return res.status(200).json({ data: timeSheets, description: "Create TimeSheet Success" })
+    const workingDay = data.workingDay
+    console.log(555, workingDay);
+    const start = moment(workingDay).startOf('day');
+    // end today
+    const end = moment(workingDay).endOf('day');
+    const lisTimeSheet = await timeSheetModel.find({ workingDay: { $gte: start, $lte: end } })
+    console.log(5555, lisTimeSheet);
+    if (lisTimeSheet.length === 0) {
+      const listPersonnel = await personnelModel.find()
+      console.log(43333, listPersonnel);
+      const data = listPersonnel.map((item) => {
+        return { personnel: item._id, workingDay: workingDay, status: '0' }
+      })
+      const timeSheets = await timeSheetModel.insertMany(data)
+      return res.status(200).json({ data: timeSheets, description: `Create TimeSheet ${moment(workingDay).format('DD/MM/YYYY')} Success` })
+    }
+    else {
+      return res.status(200).json({ data: lisTimeSheet, description: `time attendance data on ${moment(workingDay).format('DD/MM/YYYY')} was created before` })
+    }
   } catch (error) {
     return res.status(403).json(error)
   }
@@ -77,4 +96,4 @@ const getListTimeSheet = async (req, res) => {
   }
 }
 
-module.exports = { createTimeSheetMany, updateTimeSheet, deleteTimeSheet, getTimeSheet, getListTimeSheet, getListPersonnelTimeSheet,createTimeSheet }
+module.exports = { createTimeSheetMany, updateTimeSheet, deleteTimeSheet, getTimeSheet, getListTimeSheet, getListPersonnelTimeSheet, createTimeSheet }
