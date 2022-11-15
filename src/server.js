bd6c2316506ebe182b630568d91d94d35dc0c382
HuyThanh <huythanh1308bn@gmail.com>
2022-11-15 09:23:50 +0700

    update 15/11

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const {app} = require('./app')
+const { app } = require('./app')
 require('dotenv').config()
 
-const port = 4200
+const port = process.env.PORT || 4200
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
