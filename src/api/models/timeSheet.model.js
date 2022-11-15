c51cf139eef4c4a730fb644d94cad05c8efce9c9
ThànhPH <huythanh1308bn@gmail.com>
2022-11-15 23:02:26 +0700

    update 15/11

--- src/api/models/timeSheet.model.js
+++ src/api/models/timeSheet.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 const timeSheetSchema = new mongoose.Schema({
   personnel: { type: Schema.Types.ObjectId, ref: 'personnel' },
-  workingDay: Date,
-  status: Number,
+  // workingDay: Date,
+  // status: Number,
   createdAt: { type: Date, required: true, default: Date.now() },
   updatedAt: { type: Date },
 })
