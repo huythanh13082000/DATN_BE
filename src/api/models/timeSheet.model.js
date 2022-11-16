b9ed59feaf112713907ad2a6c17eb8bcb5b22315
HuyThanh <huythanh1308bn@gmail.com>
2022-11-16 09:38:12 +0700

    16/11

--- src/api/models/timeSheet.model.js
+++ src/api/models/timeSheet.model.js
@@ -4,7 +4,7 @@ const timeSheetSchema = new mongoose.Schema({
   personnel: { type: Schema.Types.ObjectId, ref: 'personnel' },
   // workingDay: Date,
   // status: Number,
-  createdAt: { type: Date, required: true, default: Date.now() },
+  createdAt: { type: Date, required: true, default: Date.now },
   updatedAt: { type: Date },
 })
 const timeSheetModel = mongoose.model('timeSheet', timeSheetSchema)
