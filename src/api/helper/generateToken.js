1fca354f61444fe5e21054417e09c5e7166181ae
ThànhPH <huythanh1308bn@gmail.com>
2022-12-04 23:45:40 +0700

    update 04/12

--- src/api/helper/generateToken.js
+++ src/api/helper/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 const generateToken = (payload) => {
-  const accessToken = jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '24h' })
+  const accessToken = jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: 15 })
   const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '24h' })
   return { accessToken, refreshToken }
 }
